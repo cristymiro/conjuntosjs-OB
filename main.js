c1c8b552c131ad98108eca9fb311f318a1bf3c0d
Cris <mirocristy@gmail.com>
2022-07-15 19:25:01 +0200

    primer commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const array = ['Cris', 'Sergio', 'Bela', 'Bucur'];
+
+const miSet = new Set(array);
+
+miSet.add('Cris');
+
+console.log(miSet);
+
+miSet.add('Javascript');
+
+console.log(miSet);
+
+
